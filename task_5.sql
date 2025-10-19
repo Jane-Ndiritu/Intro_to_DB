a80ac0f3b0afa0ab2dfb91af399e91c24097e099
jane <jayndiritu36@gmail.com>
2025-10-19 11:28:03 +0300

    Populating thr DB

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'John Doe', 'john.doe@example.com', '123 Elm St');

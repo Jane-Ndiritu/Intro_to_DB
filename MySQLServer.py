f016c14f44ffc5da51dfe325f6a3294f991b3f97
jane <jayndiritu36@gmail.com>
2025-10-19 11:09:54 +0300

    Addition of python

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,17 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="alx_book_store"
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-mycursor.execute("USE alx_book_store")
+try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="yourusername",
+      password="yourpassword",
+      database="alx_book_store"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    mycursor.execute("USE alx_book_store")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")    
+finally:
+    if 'mydb' in locals() and mydb.is_connected():
+        mycursor.close()
+        mydb.close()
